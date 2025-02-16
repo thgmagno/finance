f183c7c195c7e1f2c51833e28cc4289d2f93d3ea
thgmagno <thgmgn@gmail.com>
2025-02-16 01:04:40 -0300

    update auth middleware

--- src/auth.ts
+++ src/auth.ts
@@ -3,8 +3,13 @@ import Google from 'next-auth/providers/google'
 import GitHub from 'next-auth/providers/github'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { prisma } from '@/database/prisma'
+import { env } from 'root/env'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [GitHub, Google],
+  session: {
+    strategy: 'jwt',
+  },
+  secret: env.AUTH_SECRET,
 })
